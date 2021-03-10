5e576cb10a2cc42010999365bfe3086addbeb2c1
GungeeSpla <47924143+GungeeSpla@users.noreply.github.com>
2021-03-10 16:59:02 +0900

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,729 @@
+// =========================================================
+// main.js
+// =========================================================
+
+/** all_json_list */
+var all_json_list = {
+	/** Coop */
+	Coop: [
+		'Coop_Levels',
+		'Coop_System',
+	],
+	/** Enemy */
+	Enemy: [
+		'Enm_CoopSakediver',
+		'Enm_CoopSakediver_Action',
+		'Enm_CoopSakedozer',
+		'Enm_CoopSakedozer_Action',
+		'Enm_CoopSakedozer_Designer',
+		'Enm_CoopSakeflyer',
+		'Enm_CoopSakelienBagman',
+		'Enm_CoopSakelienBagman_Action',
+		'Enm_CoopSakelienBagmanLarge',
+		'Enm_CoopSakelienBagmanLarge_Action',
+		'Enm_CoopSakelienBomber',
+		'Enm_CoopSakelienBomber_Action',
+		'Enm_CoopSakelienBomber_Designer',
+		'Enm_CoopSakelienCannon',
+		'Enm_CoopSakelienCannon_Action',
+		'Enm_CoopSakelienCup',
+		'Enm_CoopSakelienCup_Action',
+		'Enm_CoopSakelienCupTwins',
+		'Enm_CoopSakelienCupTwins_Action',
+		'Enm_CoopSakelienCupTwins_Designer',
+		'Enm_CoopSakelienEscape',
+		'Enm_CoopSakelienEscape_Action',
+		'Enm_CoopSakelienGeyser',
+		'Enm_CoopSakelienGeyser_Action',
+		'Enm_CoopSakelienGolden',
+		'Enm_CoopSakelienGolden_Action',
+		'Enm_CoopSakelienGolden_Designer',
+		'Enm_CoopSakelienLarge',
+		'Enm_CoopSakelienLarge_Action',
+		'Enm_CoopSakelienLarge_Designer',
+		'Enm_CoopSakelienShield',
+		'Enm_CoopSakelienShield_Action',
+		'Enm_CoopSakelienSmall',
+		'Enm_CoopSakelienSmall_Action',
+		'Enm_CoopSakelienSnake',
+		'Enm_CoopSakelienSnake_Action',
+		'Enm_CoopSakelienStandard',
+		'Enm_CoopSakelienStandard_Action',
+		'Enm_CoopSakelienStandard_Designer',
+		'Enm_CoopSakelienTower',
+		'Enm_CoopSakelienTower_Action',
+		'Enm_CoopSakelienTower_Designer',
+		'Enm_CoopSakepuncher',
+		'Enm_CoopSakepuncherBulletPunch',
+		'Enm_CoopSakepuncherBulletPunch_Designer',
+		'Enm_CoopSakepuncherBulletSimpl',
+		'Enm_CoopSakepuncherBulletSimpl_Action',
+		'Enm_CoopSakepuncherBulletSimpl_Designer',
+		'Enm_CoopSakepuncher_Action',
+		'Enm_CoopSakepuncher_Designer',
+		'Enm_CoopSakerocket',
+		'Enm_CoopSakerocket_Action',
+		'Enm_CoopSakerocketBullet',
+		'Enm_CoopSakerocketBullet_Action',
+	],
+	/** WeaponBullet */
+	WeaponBullet: [
+		'AquaBall',
+		'AquaBallMission',
+		'AquaBall_Octa',
+		'BigBang',
+		'BlasterCoopBurst',
+		'BlasterCoopBurst_Burst',
+		'BlasterCoopMiddle',
+		'BlasterCoopMiddle_Burst',
+		'BlasterCoopMiddle_Burst_CoopEx',
+		'BlasterCoopShort',
+		'BlasterCoopShort_Burst',
+		'BlasterCoopShort_Burst_CoopEx',
+		'BlasterLight',
+		'BlasterLightLong',
+		'BlasterLightLong_Burst',
+		'BlasterLightLong_Octa',
+		'BlasterLightLong_Octa_Burst',
+		'BlasterLightShort',
+		'BlasterLightShort_Burst',
+		'BlasterLightShort_Octa',
+		'BlasterLightShort_Octa_Burst',
+		'BlasterLight_Burst',
+		'BlasterLight_Octa',
+		'BlasterLight_Octa_Burst',
+		'BlasterLong',
+		'BlasterLong_Burst',
+		'BlasterLong_Octa',
+		'BlasterLong_Octa_Burst',
+		'BlasterMiddle',
+		'BlasterMiddle_Burst',
+		'BlasterMiddle_Octa',
+		'BlasterMiddle_Octa_Burst',
+		'BlasterMissionMiddleLv0',
+		'BlasterMissionMiddleLv0_Burst',
+		'BlasterMissionMiddleLv1',
+		'BlasterMissionMiddleLv1_Burst',
+		'BlasterRvl0Lv0',
+		'BlasterRvl0Lv0_Burst',
+		'BlasterRvl0Lv1',
+		'BlasterRvl0Lv1_Burst',
+		'BlasterRvl1Lv0_Octa',
+		'BlasterRvl1Lv0_Octa_Burst',
+		'BlasterRvl1Lv1_Octa',
+		'BlasterRvl1Lv1_Octa_Burst',
+		'BlasterShort',
+		'BlasterShort_Burst',
+		'BlasterShort_Octa',
+		'BlasterShort_Octa_Burst',
+		'BombChase',
+		'BombCurling',
+		'BombCurlingLauncher',
+		'BombCurlingLauncherMission',
+		'BombCurlingLauncherRvl1_Octa',
+		'BombCurlingLauncher_Octa',
+		'BombCurlingRvl1_Octa',
+		'BombCurling_Octa',
+		'BombPiyo',
+		'BombPointSensor',
+		'BombPointSensor_Octa',
+		'BombPoisonBall',
+		'BombPoisonFog',
+		'BombPoisonFog_Octa',
+		'BombQuick',
+		'BombQuickLauncher',
+		'BombQuickLauncherRvl1_Octa',
+		'BombQuickLauncher_Octa',
+		'BombQuickRvl1_Octa',
+		'BombQuick_Octa',
+		'BombRobo',
+		'BombRoboLauncher',
+		'BombRoboLauncherRvl1_Octa',
+		'BombRoboLauncher_Octa',
+		'BombRoboRvl1_Octa',
+		'BombRobo_Octa',
+		'BombSplash',
+		'BombSplashLauncher',
+		'BombSplashLauncherCoop',
+		'BombSplashLauncherMission',
+		'BombSplashLauncherRvl1_Octa',
+		'BombSplashLauncher_Octa',
+		'BombSplashRvl1_Octa',
+		'BombSplash_Octa',
+		'BombSuction',
+		'BombSuctionLauncher',
+		'BombSuctionLauncher_Octa',
+		'BombSuction_Octa',
+		'BombTako',
+		'ChargerCoopLight',
+		'ChargerCoopNormal',
+		'ChargerCoopQuick',
+		'ChargerCoopSpark',
+		'ChargerEnemy',
+		'ChargerEnemy_Octa',
+		'ChargerKeeper',
+		'ChargerKeeper_Octa',
+		'ChargerLight',
+		'ChargerLight_Octa',
+		'ChargerLong',
+		'ChargerLongScope',
+		'ChargerLongScope_Octa',
+		'ChargerLong_Octa',
+		'ChargerMissionNormalLv0',
+		'ChargerMissionNormalLv1',
+		'ChargerNormal',
+		'ChargerNormalScope',
+		'ChargerNormalScope_Octa',
+		'ChargerNormal_Octa',
+		'ChargerQuick',
+		'ChargerQuick_Octa',
+		'Gachihoko',
+		'GachihokoMission',
+		'GachihokoMissionOcta',
+		'Jetpack',
+		'JetpackCoop',
+		'JetpackMission',
+		'JetpackRvl1_Octa',
+		'Jetpack_Octa',
+		'JumpBeacon',
+		'JumpBeacon_Octa',
+		'MissileMissilePosition',
+		'MissileMissilePositionCoop',
+		'MissileMissilePositionMission',
+		'MissileMissilePositionMissionOcta',
+		'RainCloud',
+		'RainCloudCoopEnemy',
+		'RainCloudMission',
+		'RainCloudRvl1_Octa',
+		'RainCloud_Octa',
+		'RollerBrushMini',
+		'RollerBrushMini_Jump',
+		'RollerBrushMini_Octa',
+		'RollerBrushMini_Octa_Jump',
+		'RollerBrushMini_Octa_Stand',
+		'RollerBrushMini_Stand',
+		'RollerBrushNormal',
+		'RollerBrushNormal_Jump',
+		'RollerBrushNormal_Octa',
+		'RollerBrushNormal_Octa_Jump',
+		'RollerBrushNormal_Octa_Stand',
+		'RollerBrushNormal_Stand',
+		'RollerBrushRvl1Lv0_Octa',
+		'RollerBrushRvl1Lv0_Octa_Jump',
+		'RollerBrushRvl1Lv0_Octa_Stand',
+		'RollerBrushRvl1Lv1_Octa',
+		'RollerBrushRvl1Lv1_Octa_Jump',
+		'RollerBrushRvl1Lv1_Octa_Stand',
+		'RollerCompact',
+		'RollerCompact_Jump',
+		'RollerCompact_Octa',
+		'RollerCompact_Octa_Jump',
+		'RollerCompact_Octa_Stand',
+		'RollerCompact_Stand',
+		'RollerCoopBrushNormal',
+		'RollerCoopBrushNormal_CoopEx',
+		'RollerCoopBrushNormal_Jump',
+		'RollerCoopBrushNormal_Stand',
+		'RollerCoopHeavy',
+		'RollerCoopHeavy_CoopEx',
+		'RollerCoopHeavy_Jump',
+		'RollerCoopHeavy_Stand',
+		'RollerCoopNormal',
+		'RollerCoopNormal_CoopEx',
+		'RollerCoopNormal_Jump',
+		'RollerCoopNormal_Stand',
+		'RollerEnemy',
+		'RollerEnemy_Jump',
+		'RollerEnemy_Stand',
+		'RollerHeavy',
+		'RollerHeavy_Jump',
+		'RollerHeavy_Octa',
+		'RollerHeavy_Octa_Jump',
+		'RollerHeavy_Octa_Stand',
+		'RollerHeavy_Stand',
+		'RollerHunter',
+		'RollerHunter_Jump',
+		'RollerHunter_Octa',
+		'RollerHunter_Octa_Jump',
+		'RollerHunter_Octa_Stand',
+		'RollerHunter_Stand',
+		'RollerKingSquid',
+		'RollerMissionBrushNormalLv0',
+		'RollerMissionBrushNormalLv0_Jump',
+		'RollerMissionBrushNormalLv0_Stand',
+		'RollerMissionBrushNormalLv1',
+		'RollerMissionBrushNormalLv1_Jump',
+		'RollerMissionBrushNormalLv1_Stand',
+		'RollerMissionNormalLv0',
+		'RollerMissionNormalLv0_Jump',
+		'RollerMissionNormalLv0_Stand',
+		'RollerMissionNormalLv1',
+		'RollerMissionNormalLv1_Jump',
+		'RollerMissionNormalLv1_Stand',
+		'RollerNormal',
+		'RollerNormal_Jump',
+		'RollerNormal_Octa',
+		'RollerNormal_Octa_Jump',
+		'RollerNormal_Octa_Stand',
+		'RollerNormal_Stand',
+		'RollerRvl0Lv0',
+		'RollerRvl0Lv0_Jump',
+		'RollerRvl0Lv0_Stand',
+		'RollerRvl0Lv1',
+		'RollerRvl0Lv1_Jump',
+		'RollerRvl0Lv1_Stand',
+		'RollerRvl1Lv0_Octa',
+		'RollerRvl1Lv0_Octa_Jump',
+		'RollerRvl1Lv0_Octa_Stand',
+		'RollerRvl1Lv1_Octa',
+		'RollerRvl1Lv1_Octa_Jump',
+		'RollerRvl1Lv1_Octa_Stand',
+		'Shield',
+		'Shield_Octa',
+		'ShooterBlaze',
+		'ShooterBlaze_Octa',
+		'ShooterCoopExpert',
+		'ShooterCoopExpert_CoopEx',
+		'ShooterCoopFirst',
+		'ShooterCoopFirst_CoopEx',
+		'ShooterCoopGravity',
+		'ShooterCoopGravity_CoopEx',
+		'ShooterCoopNormal',
+		'ShooterCoopNormal_CoopEx',
+		'ShooterCoopShort',
+		'ShooterCoopShort_CoopEx',
+		'ShooterEnemy',
+		'ShooterExpert',
+		'ShooterExpert_Octa',
+		'ShooterFirst',
+		'ShooterFirst_Octa',
+		'ShooterFlash',
+		'ShooterFlashRepeat',
+		'ShooterFlashRepeat_Octa',
+		'ShooterFlash_Octa',
+		'ShooterGravity',
+		'ShooterGravity_Octa',
+		'ShooterHeavy',
+		'ShooterHeavy_Octa',
+		'ShooterLong',
+		'ShooterLong_Octa',
+		'ShooterMissionNormalLv0',
+		'ShooterMissionNormalLv1',
+		'ShooterMissionNormalLv2',
+		'ShooterNormal',
+		'ShooterNormal_Octa',
+		'ShooterPrecision',
+		'ShooterPrecision_Octa',
+		'ShooterQuickLong',
+		'ShooterQuickLong_Octa',
+		'ShooterQuickMiddle',
+		'ShooterQuickMiddle_Octa',
+		'ShooterRvl0Lv0',
+		'ShooterRvl0Lv1',
+		'ShooterRvl0Lv2',
+		'ShooterRvl0Lv3',
+		'ShooterRvl1Lv0_Octa',
+		'ShooterRvl1Lv1_Octa',
+		'ShooterRvl2Lv0_Octa',
+		'ShooterRvl2Lv1_Octa',
+		'ShooterShort',
+		'ShooterShort_Octa',
+		'ShooterTripleMiddle',
+		'ShooterTripleMiddle_Octa',
+		'ShooterTripleQuick',
+		'ShooterTripleQuick_Octa',
+		'SlosherBathtub',
+		'SlosherCoopLauncher',
+		'SlosherCoopStrong',
+		'SlosherCoopVase',
+		'SlosherDiffusion',
+		'SlosherDiffusion_Octa',
+		'SlosherLauncher',
+		'SlosherLauncher_Octa',
+		'SlosherMissionStrongLv0',
+		'SlosherMissionStrongLv1',
+		'SlosherRvl0Lv0',
+		'SlosherRvl0Lv1',
+		'SlosherRvl1Lv0_Octa',
+		'SlosherRvl1Lv1_Octa',
+		'SlosherStrong',
+		'SlosherStrong_Octa',
+		'SlosherWashtub',
+		'SpinnerCoopStandard',
+		'SpinnerCoopStandard_2',
+		'SpinnerDownpour',
+		'SpinnerDownpourRepeat',
+		'SpinnerDownpour_2',
+		'SpinnerEnemy',
+		'SpinnerEnemy_2',
+		'SpinnerHyper',
+		'SpinnerHyper_2',
+		'SpinnerHyper_Octa',
+		'SpinnerHyper_Octa_2',
+		'SpinnerMissionStandardLv0',
+		'SpinnerMissionStandardLv0_2',
+		'SpinnerMissionStandardLv1',
+		'SpinnerMissionStandardLv1_2',
+		'SpinnerQuick',
+		'SpinnerQuick_2',
+		'SpinnerQuick_Octa',
+		'SpinnerQuick_Octa_2',
+		'SpinnerSerein',
+		'SpinnerSerein_2',
+		'SpinnerStandard',
+		'SpinnerStandard_2',
+		'SpinnerStandard_Octa',
+		'SpinnerStandard_Octa_2',
+		'Sprinkler',
+		'SprinklerMission',
+		'Sprinkler_Octa',
+		'SuperArmor',
+		'SuperArmorCoop',
+		'SuperArmorRvl1_Octa',
+		'SuperArmorRvl2_Octa',
+		'SuperArmor_Octa',
+		'SuperBall',
+		'SuperBubble',
+		'SuperBubbleEnemy_Octa',
+		'SuperBubble_Octa',
+		'SuperLanding',
+		'SuperLandingCoop',
+		'SuperLandingMission',
+		'SuperLandingRvl1_Octa',
+		'SuperLanding_Octa',
+		'SuperLaser',
+		'SuperMissile',
+		'SuperMissileEnemy_Octa',
+		'SuperMissileMission',
+		'SuperMissileRvl1_Octa',
+		'SuperMissile_Octa',
+		'SuperStamp',
+		'Trap',
+		'Trap_Octa',
+		'TwinsCoopNormal',
+		'TwinsCoopNormal_2',
+		'TwinsDual',
+		'TwinsDual_2',
+		'TwinsDual_Octa',
+		'TwinsDual_Octa_2',
+		'TwinsGallon',
+		'TwinsGallon_2',
+		'TwinsGallon_Octa',
+		'TwinsGallon_Octa_2',
+		'TwinsMissionNormalLv0',
+		'TwinsMissionNormalLv0_2',
+		'TwinsMissionNormalLv1',
+		'TwinsMissionNormalLv1_2',
+		'TwinsNormal',
+		'TwinsNormal_2',
+		'TwinsNormal_Octa',
+		'TwinsNormal_Octa_2',
+		'TwinsRvl1Lv0_Octa',
+		'TwinsRvl1Lv0_Octa_2',
+		'TwinsRvl1Lv1_Octa',
+		'TwinsRvl1Lv1_Octa_2',
+		'TwinsShort',
+		'TwinsShort_2',
+		'TwinsShort_Octa',
+		'TwinsShort_Octa_2',
+		'TwinsStepper',
+		'TwinsStepper_2',
+		'TwinsStepper_Octa',
+		'TwinsStepper_Octa_2',
+		'UmbrellaCompact',
+		'UmbrellaCompact_Octa',
+		'UmbrellaCoopAutoAssault',
+		'UmbrellaCoopNormal',
+		'UmbrellaMissionNormalLv0',
+		'UmbrellaMissionNormalLv1',
+		'UmbrellaNormal',
+		'UmbrellaNormal_Octa',
+		'UmbrellaRvl1Lv0_Octa',
+		'UmbrellaRvl1Lv1_Octa',
+		'UmbrellaWide',
+		'UmbrellaWide_Octa',
+		'VictoryClam',
+		'VictoryRocket',
+		'WaterCutter',
+		'WaterCutterCoop',
+		'WaterCutterEnemy_Octa',
+		'WaterCutterMission',
+		'WaterCutterRvl1_Octa',
+		'WaterCutter_Octa',
+	]
+};
+
+/** DOMContentLoaded */
+window.addEventListener('DOMContentLoaded', function() {
+	var buttons = [];
+	var selects = [];
+	Object.keys(all_json_list).forEach(function(dirname, i) {
+		var arr = all_json_list[dirname];
+		
+		// ボタン
+		var button = document.createElement('button');
+		button.textContent = dirname;
+		buttons.push(button);
+		document.getElementById('button').appendChild(button);
+		
+		// セレクトボックス
+		var select = document.createElement('select');
+		select.setAttribute('data-dirname', dirname);
+		arr.forEach(function(item) {
+			var option = document.createElement('option');
+			option.textContent = item;
+			option.value = item;
+			select.appendChild(option);
+		});
+		selects.push(select);
+		document.getElementById('select').appendChild(select);
+		
+		// イベントハンドラ
+		(function(dirname, select, button) {
+			
+			// セレクトのチェンジイベント
+			select.addEventListener('change', function(e) {
+				var num = select.selectedIndex;
+				var text = select.options[num].textContent;
+				var url = './assets/json/'+dirname+'/'+text+'.bprm.json';
+				get_json(url, function(json) {
+					makeTable(json);
+				});
+			});
+			
+			// ボタンのクリックイベント
+			button.addEventListener('click', function(e) {
+				buttons.forEach(function(b) {
+					b.classList.remove('selected');
+				});
+				selects.forEach(function(s) {
+					s.classList.remove('show');
+				});
+				button.classList.add('selected');
+				select.classList.add('show');
+				makeTableOfCurrentOption();
+			});
+		}(dirname, select, button));
+		
+		if (i === 0) {
+			button.classList.add('selected');
+			select.classList.add('show');
+		}
+	});
+	makeTableOfCurrentOption();
+});
+
+/** makeTableOfCurrentOption */
+function makeTableOfCurrentOption() {
+	var select = document.querySelector('select.show');
+	var dirname = select.getAttribute('data-dirname');
+	var num = select.selectedIndex;
+	var filename = select.options[num].textContent;
+	var url = './assets/json/'+dirname+'/'+filename+'.bprm.json';
+	get_json(url, function(json) {
+		makeTable(json);
+	});
+}
+
+/** makeTable */
+function makeTable(obj) {
+	document.getElementById('param').innerHTML = '';
+	var html = objectToTable(obj);
+	var table = document.createElement('table');
+	table.innerHTML = html;
+	document.getElementById('param').appendChild(table);
+}
+
+/** makeOverview */
+function makeOverview(filenames, dirname) {
+	var loaded = 0;
+	var overview_json = {};
+	var get_all_keys = function() {
+		var all_keys = [];
+		for (var filename in overview_json) {
+			Object.keys(overview_json[filename]).forEach(function(k) {
+				if (!all_keys.includes(k)) {
+					all_keys.push(k);
+				}
+			});
+		}
+		return all_keys.sort();
+	};
+	var onload = function() {
+		var all_keys = get_all_keys();
+		var html = '';
+		// ヘッダの作成
+		html += '<thead><tr>';
+		html += '<td>Parameter Name</td>';
+		filenames.forEach(function(filename) {
+			html += '<td>'+filename.replace('Enm_Coop','')+'</td>';
+		});
+		html += '</tr></thead>';
+		// ボディの作成
+		html += '<tbody>';
+		all_keys.forEach(function(key) {
+			html += '<tr>';
+			html += '<td>'+parseKey(key)+'</td>';
+			filenames.forEach(function(filename) {
+				if (key in overview_json[filename]) {
+					html += '<td>'+overview_json[filename][key]+'</td>';
+				} else {
+					html += '<td>-</td>';
+				}
+			});
+			html += '</tr>';
+		});
+		html += '</tbody>';
+		document.getElementById('param').innerHTML = '';
+		var table = document.createElement('table');
+		table.innerHTML = html;
+		document.getElementById('param').appendChild(table);
+	};
+	filenames.forEach(function(name) {
+		var url = './assets/json/'+dirname+'/'+name+'.bprm.json';
+		get_json(url, function(json) {
+			overview_json[name] = json;
+			loaded++;
+			if (loaded === filenames.length) {
+				onload();
+			}
+		});
+	});
+}
+
+/** sortKeys */
+function sortKeys(keys) {
+	var dic = {};
+	keys.forEach(function(key) {
+		dic[key] = parseKey(key);
+	});
+	keys.sort(function(a, b) {
+		return (dic[a] > dic[b]) ? 1 : -1;
+	});
+	return keys;
+}
+
+/** parseKey */
+function parseKey(key) {
+	if (key.charAt(1) === 'm') {
+		return key.substr(2);
+	} else if (key.charAt(0) === 'm') {
+		return key.substr(1);
+	} else {
+		return key;
+	}
+}
+
+/** getDepth */
+function getDepth(obj, depth=1) {
+	var depthMax = depth;
+	Object.keys(obj).forEach(function(key) {
+		if (typeof obj[key] === 'object') {
+			var d = getDepth(obj[key], depth + 1);
+			if (depthMax < d) {
+				depthMax = d;
+			}
+		}
+	});
+	return depthMax;
+}
+
+/** objectToTable */
+function objectToTable(obj) {
+	var depth = getDepth(obj, 1);
+	var html = '';
+	html += '<thead><tr>';
+	html += '<td colspan="'+(depth)+'">Parameter Name</td>';
+	html += '<td>Value</td>';
+	html += '</tr></thead>';
+	html += '<tbody>'
+	var count = {};
+	var recursive = function(val, keys) {
+		var html = '';
+		if (typeof val === 'object') {
+			sortKeys(Object.keys(val)).forEach(function(key) {
+				html += recursive(val[key], keys.concat(key));
+			});
+		} else {
+			html += '<tr>';
+			keys.forEach(function(key, i) {
+				var c = count;
+				for (var j = 0; j <= i; j++) {
+					if (!c[keys[j]]) {
+						c[keys[j]] = {};
+					}
+					c = c[keys[j]];
+				}
+				if (!c.__count) {
+					c.__count = 0;
+				}
+				c.__count++;
+				var classname, colspan;
+				if (c.__count <= 1) {
+					classname = 'param-name first';
+				} else {
+					classname = 'param-name not-first';
+				}
+				if (i + 1 < keys.length) {
+					colspan = 1;
+				} else {
+					colspan = depth - i;
+				}
+				html += '<td class="'+classname+'" colspan="'+colspan+'">' + parseKey(key) + '</td>';
+			});
+			html += '<td>' + val + '</td>';
+			html += '</tr>';
+		}
+		return html;
+	};
+	sortKeys(Object.keys(obj)).forEach(function(key) {
+		html += recursive(obj[key], [key]);
+	});
+	html += '</tbody>'
+	return html;
+}
+/*
+function objectToTable(obj) {
+	var html = '<table>';
+	Object.keys(obj).forEach(function(key) {
+		html += '<tr>';
+		html += '<td>' + key + '</td>';
+		var type = typeof obj[key];
+		if (typeof obj[key] === 'object') {
+			html += '<td>' + objectToTable(obj[key]) + '</td>';
+		} else {
+			html += '<td>' + obj[key] + '</td>';
+		}
+		html += '</tr>';
+	});
+	html += '</table>';
+	return html;
+}
+*/
+
+/** get_json(url, callback) */
+var json_cache = {};
+function get_json(url, callback) {
+	if (json_cache[url]) {
+		if (callback) {
+			callback(json_cache[url]);
+		}
+		return;
+	}
+	var req = new XMLHttpRequest();
+	req.onreadystatechange = function() {
+		if (req.readyState === 4) {
+			if (req.status === 200) {
+				var json = JSON.parse(req.responseText);
+				json_cache[url] = json;
+				if (callback) {
+					callback(json);
+				}
+			}
+		}
+	};
+	req.open('GET', url);
+	req.send(null);
+}
+
+/** end */
